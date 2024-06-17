ca75930edd7f49985ca011a7294d5937e2c0e3d0
Justin Li <justinli@Justins-MacBook-Pro.local>
2024-06-17 14:15:11 -0400

    com

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ app.mount('#app');
 
 AOS.init({
   duration: 5000, // animation duration in ms
-  offset: 200, // offset (in px) from the original trigger point
+  offset: 500, // offset (in px) from the original trigger point
   easing: 'ease-in-out', // easing function
   once: false, // whether animation should happen only once
   mirror: true,
